83899ab81d87c12e9ad16dfe6006c9ed076af07e
Shazad Naviwala <shazad@weka.io>
2023-05-19 08:20:02 -0500

    new checker

--- weka_upgrade_checker/weka_upgrade_checker.py
+++ weka_upgrade_checker/weka_upgrade_checker.py
@@ -22,7 +22,7 @@ if sys.version_info < (3, 8):
     print("Must have python version 3.8 or later installed.")
     sys.exit(1)
 
-pg_version = "1.2.5"
+pg_version = "1.2.6"
 
 log_file_path = os.path.abspath("./weka_upgrade_checker.log")
 
@@ -1277,7 +1277,9 @@ supported_os = {
                 "20.04.0",
                 "20.04.1",
                 "20.04.2",
-                "20.04.3"
+                "20.04.3",
+                "20.04.4",
+                "20.04.5"
             ],
             "amzn": [
                 "17.09",
@@ -1793,7 +1795,7 @@ def backend_host_checks(backend_hosts, ssh_bk_hosts, weka_version, check_version
     results = parallel_execution(
         ssh_bk_hosts,
         [
-            f'for name in $(weka local ps --no-header -o name| egrep -v "samba|smbw|s3|ganesha|envoy"); do du -sm {data_dir}"$name"_{weka_version}; done'
+            f'for name in $(weka local ps --no-header -o name,versionName| egrep -v "samba|smbw|s3|ganesha|envoy" | tr -s " " "_"); do du -sm {data_dir}"$name"; done'
         ],
         use_check_output=True, ssh_identity=ssh_identity
     )
