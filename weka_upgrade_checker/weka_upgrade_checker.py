8f9d0e0e741a1ee68ea1b1809c1731feb9ec448d
Jack Challen <jack.challen@weka.io>
2023-06-07 17:50:28 +0100

    Version bump as requested to match VT-57 change

--- weka_upgrade_checker/weka_upgrade_checker.py
+++ weka_upgrade_checker/weka_upgrade_checker.py
@@ -22,7 +22,7 @@ if sys.version_info < (3, 8):
     print("Must have python version 3.8 or later installed.")
     sys.exit(1)
 
-pg_version = "1.2.7"
+pg_version = "1.2.8"
 
 log_file_path = os.path.abspath("./weka_upgrade_checker.log")
 
