5a30bac18023acb696839524923f72d3fa60f599
Vince Fleming <vince@weka.io>
2024-02-09 08:07:21 -0800

    touch-ups

--- install/scripts.d/ta/530_high_drive_read_ssd_ratio.sh
+++ install/scripts.d/ta/530_high_drive_read_ssd_ratio.sh
@@ -2,7 +2,7 @@
 
 #set -ue # Fail with an error code if there's any sub-command/variable error
 
-DESCRIPTION="Examine the ratio of NVMe read operations to DRIVE node read operations"
+DESCRIPTION="Ratio of NVMe read operations to DRIVE node read operations"
 SCRIPT_TYPE="single"
 JIRA_REFERENCE="WEKAPP-335035"
 WTA_REFERENCE=""
