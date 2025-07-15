0386481ed5f1d628366630c48d65ed7628d7827d
Sean Ashton <sean.ashton@weka.io>
2025-07-15 12:14:15 +1000

    Fixed version detection Added remote SSH user configuration option Improved documentation in rolling_reboots.sh

--- utils/rolling_reboots.sh
+++ utils/rolling_reboots.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
-#Reboots all hosts - waits for rebuild before continuing and accepts a number of drive failures
-#Logs results
-#Requires passwordless root ssh
-#Configuration:
-HOST_LIST=./hosts
-DRIVE_FAILURE_MAX=0 #Number of failed drives to tolerate
-MIN_UPTIME=0 #Minutes - Skip recently rebooted hosts
-S3_DRAIN_TIME=90 #Seconds
-COMMENT_COMPLETED=true #Comment out host once complete as a checkpointing system.
-CHECK_INTERVAL=30 #Seconds between drive/ssh/weka status checks.
-CHECK_COUNT_LIMIT=5 #Times to check drives before rebooting again.
-DRY_RUN=false #If true: don't actually drain S3 and reboot.
-SKIP_IF_OFFLINE=true #Allow skipping hosts that are offline before we have interacted with them
-LOG_DRIVE_IDS=true #Log Failed drive IDs
-TRY_PCIE_RESCAN=true #Try rescanning PCIe bus before restarting on failed drives. Use only on actual NVMe drives
+#Safely serially reboot all hosts present in provided hosts file. See configuration section for options
+#Waits for S3 to drain then reboots host; logs results to file for review
+#Requires passwordless ssh with sudo priviledges
+
+##### Configuration #####
+HOST_LIST=./hosts           #File containing hostnames to reboot - 1 per line. Allows skipping hosts by prepending a line with #
+DRIVE_FAILURE_MAX=0         #Number of failed drives to tolerate, otherwise reboot the host again
+MIN_UPTIME=0                #Minutes - Skip recently rebooted hosts
+S3_DRAIN_TIME=90            #Seconds - Minimum time to wait for S3 to finish draining before rebooting anyway
+COMMENT_COMPLETED=true      #Comment out host in HOST_LIST file once complete to allow continuing interupted runs
+CHECK_INTERVAL=30           #Seconds between drive/ssh/weka status checks
+CHECK_COUNT_LIMIT=5         #Number of times to check drives before rebooting again if more drives down than specified by DRIVE_FAILURE_MAX
+DRY_RUN=false               #If true: don't actually drain S3 and reboot.
+SKIP_IF_OFFLINE=true        #Allow skipping hosts that are offline before we have interacted with them
+LOG_DRIVE_IDS=true          #Log Failed drive IDs to the log file and console
+TRY_PCIE_RESCAN=true        #Try rescanning PCIe bus before restarting on failed drives. ***Use only on NVMe drives***
 SKIP_ON_S3_DRAIN_FAIL=false #If S3 fails to drain then skip host instead of rebooting
 
+SSH_USER="$(whoami)"        #Specify remote username
 LOG_FILE="$(basename "$0" | sed 's/\.[^.]*$//')_$(date '+%Y%m%d_%H%M%S').log"
 
-trap clean_exit INT TERM
-set -euo pipefail
+##### END CONFIGURATION #####
+
+trap clean_exit INT TERM #Catch CTRL+C and exit cleanly
+set -euo pipefail        #Exit on errors & unset variable use
 
 log() {
 	local message="$1"
@@ -39,10 +43,11 @@ clean_exit() {
 wait_for_ssh() {
 	local host="$1"
 	log "WAITING:    for $host to be reachable over ssh"
+	sleep "$CHECK_INTERVAL"
 
 	while true; do
-	# Attempt to connect via SSH
-		if ssh -o ConnectTimeout=5 -o BatchMode=yes "$host" "weka status" >/dev/null 2>&1; then
+	# Attempt to connect via SSH and run a weka command
+		if ssh -l "$SSH_USER" -o ConnectTimeout=5 -o BatchMode=yes "$host" "weka status" >/dev/null 2>&1; then
 			log "CONTINUING: $host is online."
 			return 0
 		else
@@ -53,14 +58,14 @@ wait_for_ssh() {
 }
 
 wait_for_weka_ok() {
-	#Loop infinitely as we must not continue if WEKA status is not OK, this can take hours depending on rebuilds.
+	#Loop indefinitely as we must not continue if WEKA status is not OK, this can take hours depending on rebuilds
 	local host="$1"
-	log "WAITING:    for WEKA Cluster to return to Status: OK"
+	log "WAITING:    WEKA Cluster is not Status: OK"
 	while true; do
 		local status_output
-		status_output=$(ssh "$host" 'weka status' 2>/dev/null)
+		status_output=$(ssh -l "$SSH_USER" "$host" 'weka status' 2>/dev/null)
 		local status_line
-		status_line=$(echo "$status_output" | grep -m 1 '^ *status: ' | sed 's/^ *s/S/' || echo "status: unknown")
+		status_line=$(echo "$status_output" | grep -m 1 '^ *status: ' | sed 's/^ *s/S/' || echo "Status: Unknown")
 		if echo "$status_line" | grep -q 'Status: OK'; then
 			log "CONTINUING: $status_line"
 			return 0  # Exit the function if status is OK
@@ -76,11 +81,12 @@ pcie_rescan() {
 	local drive_info=$2
 	local new_info=$3
 	local failed
+	#Get current failed drives for host
 	failed=$(echo "$new_info" | grep -vwe ACTIVE -e PHASING_IN -e HOSTNAME || true)
 	echo "$failed" | while IFS= read -r drive; do
 		local uid
-		uid=$(echo "$drive" | awk '{print $2}')
-		log "DRIVE:      Processing failed drive with UID: $uid" #parse $drive down to just uid
+		uid=$(echo "$drive" | awk '{print $2}') #parse $drive down to just uid
+		log "DRIVE:      Processing failed drive with UID: $uid"
 
 		local pci_data
 		pci_data=$(echo "$drive_info" | awk -v uid="$uid" '$2 == uid {print $0}')
@@ -94,20 +100,19 @@ pcie_rescan() {
 				log "DRIVE:      Host safety check passed; Removing drive $uid"
 				#Check if failed drive ID is on the PCIe bus: ls /sys/bus/pci/devices/<id>
 				#If on the bus, remove: echo "1" > /sys/bus/pci/devices/<id>/remove
-				# Execute SSH command and use command substitution to capture the result
 				local ssh_output
-				ssh_output=$(ssh -n "$host" "[ -d /sys/bus/pci/devices/$pci_id ] && echo 'present' || echo 'absent'")
+				ssh_output=$(ssh -l "$SSH_USER" -n "$host" "[ -d /sys/bus/pci/devices/$pci_id ] && echo 'present' || echo 'absent'")
 
 				if [ "$ssh_output" == "present" ]; then
 					log "DRIVE:      $uid is present in the PCIe bus. Removing it now."
-					ssh -n "$host" "echo '1' | sudo tee /sys/bus/pci/devices/$pci_id/remove > /dev/null"
+					ssh -l "$SSH_USER" -n "$host" "echo '1' | sudo tee /sys/bus/pci/devices/$pci_id/remove > /dev/null"
 				else
 					log "DRIVE:      $uid is not present in the PCIe bus, a reboot may be required."
 				fi
 			else
 				log "DRIVE:      Host mismatch for UID: $uid. Expected: $host, Found: $drive_host"
 				log "FATAL:      Drive host mismatched, exiting!"
-				echo -e "\a" #Ring the bell
+				echo -e "\a" #Ring the bell on fatal error
 				exit 3
 			fi
 		else
@@ -115,7 +120,7 @@ pcie_rescan() {
 		fi
 	done
 	log "DRIVE:      Rescanning PCI bus"
-	ssh "$host" "echo '1' | sudo tee /sys/bus/pci/rescan > /dev/null"
+	ssh -l "$SSH_USER" "$host" "echo '1' | sudo tee /sys/bus/pci/rescan > /dev/null"
 }
 
 wait_for_drain() {
@@ -128,10 +133,10 @@ wait_for_drain() {
 	local connections
 	while [ "$(date +%s)" -lt $end_time ];do
 		sleep $CHECK_INTERVAL
-		if echo "$version" | grep "4\.[0-3]\." > /dev/null;then
-			connections=$(ssh "$host" "curl -s http://localhost:9001/minio/reqswatermark/currentreqs | grep -o '[0-9]*'") #Pre-4.4
+		if echo "$version" | grep "^4\.[0-3]\." > /dev/null;then
+			connections=$(ssh -l "$SSH_USER" "$host" "curl -s http://localhost:9001/minio/reqswatermark/currentreqs | grep -o '[0-9]*'") #Pre-4.4
 			else
-			connections=$(ssh "$host" 'weka s3 cluster status -o requests -F hostname=$(hostname) --no-header') #4.4+
+			connections=$(ssh -l "$SSH_USER" "$host" 'weka s3 cluster status -o requests -F hostname=$(hostname) --no-header') #4.4+
 		fi
 		if [ "$connections" = 0 ]; then
 			log "INFO:       S3 finished draining on $host"
@@ -144,7 +149,7 @@ wait_for_drain() {
 
 reboot_host() {
 	local host=$1
-	ssh -o ServerAliveInterval=1 -o ServerAliveCountMax=5 "$host" "
+    ssh -l "$SSH_USER" -o ServerAliveInterval=1 -o ServerAliveCountMax=5 "$host" "
 	(sleep 1 && sudo shutdown -r now &>/dev/null &) &
 	exit 0" &>/dev/null || true
 }
@@ -162,42 +167,44 @@ for host in "${HOSTS[@]}"; do
 	if [[ "${host%%.*}" == "${local_hostname%%.*}" ]]; then log "SKIPPING:   $host due to it being localhost"; skipped=$((skipped + 1)); continue; fi
 	#Skip if commented out
 	if [[ $host == \#* ]]; then log "SKIPPING:   $host due to leading #"; skipped=$((skipped + 1)); continue; fi
-	if ! ssh -o ConnectTimeout=5 -o BatchMode=yes "$host" "exit" 2>/dev/null;then
+	if ! ssh -l "$SSH_USER" -o ConnectTimeout=5 -o BatchMode=yes "$host" "exit" 2>/dev/null;then
 		log "ERROR:      $host is offline"
 		if [ "$SKIP_IF_OFFLINE" != true ];then
 			log "FATAL:      Host is offline, set SKIP_IF_OFFLINE in the configuration section to allow continuing in this case"
-			echo -e "\a" #Ring the bell
+			echo -e "\a" #Ring the bell on fatal error
 			exit 1;
 		else
+			log "SKIPPING:   Host is offline, SKIPPING"
+			skipped=$((skipped + 1))
 			continue
 		fi
 	fi
 	#Check uptime
-	uptime_minutes=$(ssh "$host" "awk '{print int(\$1/60)}' /proc/uptime")
+	uptime_minutes=$(ssh -l "$SSH_USER" "$host" "awk '{print int(\$1/60)}' /proc/uptime")
 	if [ "$uptime_minutes" -lt "$MIN_UPTIME" ]; then
 		log "SKIPPING:   $host due to host uptime being less than $MIN_UPTIME minutes."
 		skipped=$((skipped + 1))
 		continue
 	fi
 	#Get current drive count
-	version=$(ssh "$host" "weka version current")
-	drive_count=$(ssh "$host" 'weka cluster drive -F hostname=$(hostname),status=ACTIVE --no-header | wc -l' 2>/dev/null)
+	version=$(ssh -l "$SSH_USER" "$host" "weka version current")
+	drive_count=$(ssh -l "$SSH_USER" "$host" 'weka cluster drive -F hostname=$(hostname),status=ACTIVE --no-header | wc -l' 2>/dev/null)
 	status=wait_for_ok
 	#Start loop
 	#Check WEKA Status
 	wait_for_weka_ok "$host"
 	while [ "$status" != "good" ]; do
 		#Save drive locations
-		drive_info=$(ssh "$host" 'weka cluster drive -F hostname=$(hostname) -o id,uid,path,hostname,status,serial')
+		drive_info=$(ssh -l "$SSH_USER" "$host" 'weka cluster drive -F hostname=$(hostname) -o id,uid,path,hostname,status,serial')
 		#Drain S3 & reboot
 		if [ "$DRY_RUN" = false ];then
-			s3_running=$(ssh "$host" 'if weka local status s3 &>/dev/null;then echo true;else echo false;fi')
+			s3_running=$(ssh -l "$SSH_USER" "$host" 'if weka local status s3 &>/dev/null;then echo true;else echo false;fi')
 			if [ "$s3_running" = true ];then
 				log "---LIVE---: Draining S3 of $host"
-				if echo "$version" | grep "4\.[0-3]\." > /dev/null;then #Pre-4.4
-					ssh -o ServerAliveInterval=1 -o ServerAliveCountMax=5 "$host" 'weka s3 cluster drain $(weka cluster container -F hostname=$(hostname) -o id,container --no-header | awk "/frontend/ {print \$1}")'
+				if echo "$version" | grep "^4\.[0-3]\." > /dev/null;then #Pre-4.4
+					ssh -l "$SSH_USER" "$host" 'weka s3 cluster drain $(weka cluster container -F hostname=$(hostname) -o id,container --no-header | awk "/frontend/ {print \$1}")'
 				else #4.4+
-					ssh -o ServerAliveInterval=1 -o ServerAliveCountMax=5 "$host" 'weka s3 cluster drain $(weka s3 cluster status -F hostname=$(hostname) -o id --no-header)'
+					ssh -l "$SSH_USER" "$host" 'weka s3 cluster drain $(weka s3 cluster status -F hostname=$(hostname) -o id --no-header)'
 				fi
 				if wait_for_drain "$host" "$version";then
 					log "---LIVE---: Host $host drained; rebooting"
@@ -215,24 +222,21 @@ for host in "${HOSTS[@]}"; do
 				fi
 			else
 				log "---LIVE---: No S3 running; rebooting $host"
-				ssh -o ServerAliveInterval=1 -o ServerAliveCountMax=5 "$host" "
-(sleep 1 && sudo shutdown -r now &>/dev/null &) &
-exit 0" || true
-					rebooted=$((rebooted + 1))
+				reboot_host "$host"
+				rebooted=$((rebooted + 1))
 			fi
 		else
 			log "DRY RUN:    Drain and reboot $host here"
-			ssh "$host" "echo \$(hostname) Frontend ID: \$(weka cluster container -F hostname=\$(hostname) -o id,container --no-header | awk '/frontend/ {print \$1}')"
+			ssh -l "$SSH_USER" "$host" "echo \$(hostname) Frontend ID: \$(weka cluster container -F hostname=\$(hostname) -o id,container --no-header | awk '/frontend/ {print \$1}')"
 		fi
 		#Wait for reboot
-		sleep "$CHECK_INTERVAL"
 		wait_for_ssh "$host"
 		#Check drive count
 		status=checking_drives
 		sleep 3
 		for ((j = 0; j < CHECK_COUNT_LIMIT; j++)); do
 			for ((i = 0; i < CHECK_COUNT_LIMIT; i++)); do
-				new_drive_info=$(ssh "$host" 'weka cluster drive -F hostname=$(hostname) -o id,uid,path,hostname,status,serial')
+				new_drive_info=$(ssh -l "$SSH_USER" "$host" 'weka cluster drive -F hostname=$(hostname) -o id,uid,path,hostname,status,serial')
 				new_drives_count=$(echo "$new_drive_info" | grep -cwe "ACTIVE" -e "PHASING_IN" || true 2>/dev/null)
 				drive_failures=$((drive_count - new_drives_count))
 				if [ "$drive_failures" -gt "$DRIVE_FAILURE_MAX" ]; then
