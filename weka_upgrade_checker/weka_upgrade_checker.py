557e93baacd946319cbab33dce81c8a25c70c7cc
Vincent Ragosta <vincent.ragosta@weka.io>
2025-12-15 09:46:33 -0500

    Code cleanup and refactoring
    -Add 3024.3.22.9 to SMBW patch check

--- weka_upgrade_checker/weka_upgrade_checker.py
+++ weka_upgrade_checker/weka_upgrade_checker.py
@@ -2785,7 +2785,7 @@ def backend_host_checks(
                     f"{smb_host['ip']} - {smb_host['version']} - {smb_host['mode']} - {smb_host['tsmb_version']}"
                 )
 
-            if smb_host['tsmb_version'] == "3024.3.22.7":
+            if smb_host['tsmb_version'] in ("3024.3.22.7", "3024.3.22.9"):
                 BAD(
                     f"SMBW Host {smb_host['hostname']} is running patched version ({smb_host['tsmb_version']}) "
                     f"of tsmb-server -- please review with WEKA Customer Success"
